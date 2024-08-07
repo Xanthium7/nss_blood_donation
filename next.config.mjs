e3c6299d74cd105ed98157b067090a41f8f62ebf
Alan Francis <alan.prime567@gmail.com>
2024-08-08 02:42:57 +0530

    feat: converted to pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// Configuration options for Next.js
 
-export default nextConfig;
+import withPWA from 'next-pwa';
+const nextConfig = {
+    reactStrictMode: true, 
+    swcMinify: true,    
+    compiler: {
+      removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
+    },
+};
+
+
+// Configuration object tells the next-pwa plugin 
+const pwaConfig = {
+    dest: "public", // Destination directory for the PWA files
+    // disable: process.env.NODE_ENV === "development", // Disable PWA in development mode for 
+    register: true, // Register the PWA service worker
+    skipWaiting: true, // Skip waiting for service worker activation
+};
+
+// Export the combined configuration for Next.js with PWA support
+export default withPWA(pwaConfig)(nextConfig);
