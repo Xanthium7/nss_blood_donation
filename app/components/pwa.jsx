7cbc426170e9b3e71f4304eb7a241a19bc960b81
Alan Francis <alan.prime567@gmail.com>
2024-08-10 21:19:28 +0530

    fix: loading issue and pwa service workers

---
+++ app/components/pwa.jsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React, { useEffect } from "react";
+
+function Pwa() {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+        navigator.serviceWorker.register("/sw.js").then(
+          function (registration) {
+            console.log(
+              "ServiceWorker registration successful with scope: ",
+              registration.scope
+            );
+          },
+          function (err) {
+            console.log("ServiceWorker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, []);
+
+  return <></>;
+}
+
+export default Pwa;
